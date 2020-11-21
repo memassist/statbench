688646de95347aaa0de996f6ac86182d75965553
Christos <987149+christosle@users.noreply.github.com>
2020-11-22 00:09:03 +0200

    Make unit tests fail on purpose

--- tests/Unit/SampleTest.php
+++ tests/Unit/SampleTest.php
@@ -4,10 +4,15 @@ namespace Statbench\Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
+use Statbench\Params\ParamsConsole;
+
 class SampleTest extends TestCase
 {
     public function testTrueAssertsToTrue()
     {
+        $params_console = new ParamsConsole();
+        $this->assertEquals('', $params_console->getExecutable());
+        $this->assertEquals('', $params_console->getCommand());
         $this->assertTrue(true);
     }
 }
