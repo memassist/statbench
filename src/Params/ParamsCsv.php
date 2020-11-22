d98a601cc9ab53ab41bad67c9e94e6df4bfda474
Christos <987149+christosle@users.noreply.github.com>
2020-11-22 03:46:41 +0200

    Add the input_filename member in ParamsCsv

--- src/Params/ParamsCsv.php
+++ src/Params/ParamsCsv.php
@@ -4,14 +4,17 @@ namespace Statbench\Params;
 
 class ParamsCsv
 {
-    private bool $reduction = false;    // Calculate reduction instead of speedup (ex. for cache-misses measure instead of execution time)
-    private ?string $input_file = null; // The input CSV file. null if
+    private bool $reduction = false;        // Calculate reduction instead of speedup (ex. for cache-misses measure instead of execution time)
+    private ?string $input_filename = null; // The input CSV file. null if not in 'file' mode
 
     public function __construct(
-        ?bool $reduction = null
+        ?bool $reduction = null,
+        ?string $input_filename = null
     ) {
-        $this->reduction = $reduction ?? $this->reduction;
+        $this->reduction      = $reduction      ?? $this->reduction;
+        $this->input_filename = $input_filename ?? $this->input_filename;
     }
 
-    public function getReduction() : bool { return $this->reduction; }
+    public function getReduction() :     bool    { return $this->reduction;      }
+    public function getInputFilename() : ?string { return $this->input_filename; }
 }
